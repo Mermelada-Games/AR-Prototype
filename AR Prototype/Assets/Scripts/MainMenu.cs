dc3fbb91c3cebc00fa1ed4224b5ce34f1e0dd68f
Lluccib <125250409+Lluccib@users.noreply.github.com>
2025-04-21 13:35:56 +0200

    fix scenes
    Delete main menu buttons
    
    Delete screen prefab in game scene

--- AR Prototype/Assets/Scripts/MainMenu.cs
+++ AR Prototype/Assets/Scripts/MainMenu.cs
@@ -3,22 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject panelHowToPlay;
-    public GameObject panelSettings;
-
     public void StartGame()
     {
         SceneManager.LoadScene("TestScene");
     }
-
-    public void ShowHowToPlay()
-    {
-        panelHowToPlay.SetActive(true);
-    }
-
-    public void CloseHowToPlay()
-    {
-        panelHowToPlay.SetActive(false);
-    }
-
 }
